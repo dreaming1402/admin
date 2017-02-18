760b91a04efa9ebc978cf8fa4397461508fefec6
dreaming1402 <dreaming.1402@gmail.com>
2017-02-18 15:56:07 +0700

    1.0.0.0
    First commit

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php if (!defined('PATH_SYSTEM')) die ('Bad requested!');
+
+return [
+	'DB_HOST'		=> 'localhost',
+	'DB_USER'		=> 'root',
+	'DB_PASSWORD'	=> '',
+	'DB_TABLE'		=> 'admin',
+	'DB_PREFIX'		=> 'system_',
+
+	'DEBUG_ENABLE'	=> true,
+
+	'APP_NAME'		=> 'Admin',
+	'DOMAIN_TEXT'	=> 'LongThai',
+	'BASE_URL'		=> '/pidn/admin',
+	
+	'LANGUAGE'		=> 'vi',
+];
